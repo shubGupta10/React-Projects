f24114edc5424a6385d5aae0fa8f2003e699bcb8
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-02-02 23:21:36 +0530

    New Commit

---
+++ MiniProjects/01minProject/src/App.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [facts, setFacts] = useState(null);
+  const [initialRender, setInitialRender] = useState(true);
+  const [Image, setImage] = useState(null)
+
+  const fetchcatFacts = () => {
+    fetch("https://catfact.ninja/fact")
+      .then((res) => res.json())
+      .then((data) => {
+        setFacts(data.fact);
+      });
+  }
+
+  const randomImage = () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((res) => res.json())
+    .then((imageData) => {
+       setImage(imageData.message);
+    })
+  }
+
+  useEffect(() => {
+
+    (initialRender) ? setInitialRender(false) : randomImage();   
+  }, [initialRender])
+
+  useEffect(() => {
+
+    (initialRender) ? setInitialRender(false) : fetchcatFacts();
+
+   
+  } ,[initialRender]);
+
+  return (
+    <>
+      <h1>Fact About Cats  </h1>
+      <button onClick={fetchcatFacts}>Click here to Generate Facts</button>
+      <h2 > here is the fact : {facts}</h2>
+
+
+      <img src={Image} width={500} alt="No Image" />
+      <button onClick={randomImage}>Click here to generate</button>
+    </>
+  );
+}
+
+export default App;

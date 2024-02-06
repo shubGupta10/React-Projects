2c7a060bcd6e7066e2e0e5051eda14aaa80fd6f4
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-02-06 16:05:17 +0530

    6th Feb Commit

--- BlogPoint/src/App.jsx
+++ BlogPoint/src/App.jsx
@@ -1,14 +1,46 @@
-
+import {useDispatch} from 'react-redux';
+import { useState, useEffect } from 'react'
 import './App.css'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import { Header, Footer } from './components';
+import {Outlet} from 'react-router-dom';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
 
-  return (
-    <>
-    <h1>This is a Blog Website in Appwrite</h1>
-    </>
-  )
+
+  const [loading, setLoading] = useState(true); //we are creating this usestate to show a loading screen
+  const dispatch = useDispatch()
+
+
+  //ab jaise hi loading ho then ek use effect lo and ask it to whether login or not.
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      } else{
+        dispatch((logout()))
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+
+
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400' >
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
 
 export default App

948ffdbda93474819e30c09ae964d18abc70889b
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-02-22 00:26:44 +0530

    New Commits

--- BlogPoint/src/components/index.js
+++ BlogPoint/src/components/index.js
@@ -3,6 +3,11 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container"
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import SignUp from './Signup';
+import Login from './Login';
+import PostForm from './Post-Form'
+import PostCard from './PostCard'
+import AuthLayout from './AuthLayout'
 
 export {
     Header,
@@ -10,5 +15,10 @@ export {
     Container,
     Logo,
     LogoutBtn,
-    RTE
+    RTE,
+    SignUp,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout
 }

f5c3679da8afb3833b761d6a3ed688b0fe527858
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-02-22 23:06:11 +0530

    Bugs Fixes

--- BlogPoint/src/components/index.js
+++ BlogPoint/src/components/index.js
@@ -3,11 +3,14 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container"
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from './RTE';
 import SignUp from './Signup';
 import Login from './Login';
-import PostForm from './Post-Form'
+import Button from './Button'
+import PostForm from './Post-Form/PostForm';
 import PostCard from './PostCard'
 import AuthLayout from './AuthLayout'
+import Input from './Input'
 
 export {
     Header,
@@ -18,7 +21,10 @@ export {
     RTE,
     SignUp,
     Login,
+    Button,
     PostForm,
     PostCard,
-    AuthLayout
+    AuthLayout,
+    Input,
+    Select,
 }

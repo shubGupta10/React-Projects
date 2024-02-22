01eed1922d4d5e2883cae49eb854b8a19e423c5d
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-02-23 00:07:43 +0530

    Bug Fixes

--- BlogPoint/src/pages/Home.jsx
+++ BlogPoint/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Container, PostCard } from '../components'
 
 
 function Home() {
-    const [posts , setPosts] = useState()
+    const [posts , setPosts] = useState([])
     useEffect(() => {
         appwriteService.getPosts().then((posts) => {
             if (posts) {

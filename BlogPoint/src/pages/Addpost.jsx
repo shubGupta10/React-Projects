8322bb63575eff9cddab31592724ea18d8482d67
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-02-24 19:13:12 +0530

    Bug Fixes

--- BlogPoint/src/pages/Addpost.jsx
+++ BlogPoint/src/pages/Addpost.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container, PostForm } from '../components'
 
-function Addpost() {
+function AddPost() {
   return (
     <div className='py-8'>
         <Container>
@@ -11,4 +11,4 @@ function Addpost() {
   )
 }
 
-export default Addpost
+export default AddPost
